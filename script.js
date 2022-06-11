4bfbacd423664644d60f4880ae3bddc39e2f0944
feet <weldersilvasurf@gmail.com>
2022-06-11 13:47:21 -0300

    Trabalho condicionais

---
+++ script.js
@@ -0,0 +1,40 @@
+let idade = 19;
+
+if(idade >=19){
+    console.log("Idade é maior que 18");
+}
+
+else{
+    console.log("Idade é menor que 18");
+}
+
+console.log(32 >= 18)
+
+let mesAniversario = "janeiro";
+
+if(mesAniversario == "janeiro" || mesAniversario == "dezembro" ){
+    console.log("Meu aniversario é em Janeiro");
+}
+else{
+    console.log("Meu aniversario não é Dezembro");
+}
+
+let meuNome ="W";
+
+if(meuNome == "W" || meuNome == "R" ){
+    console.log("Meu nome começa com W");
+}
+
+else{
+    console.log("Meu nome não começa com R");
+}
+
+let sobreNome ="7 letras";
+
+if(sobreNome == "7 letras" || sobreNome == "6 letras" ){
+    console.log("Meu sobrenome tem 7 letras");
+}
+
+else{
+    console.log("Meu sobrenome não tem 6 letras");
+}
